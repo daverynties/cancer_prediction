128d9da4749f11d79c71879b40ba4a82d9a229df
lucas <lucasausbury@gmail.com>
2016-12-05 10:32:21 -0500

    added training/test split
    was previously just retesting entire dataset which would of course give us a higher accuracy rate because there would be no unknowns. Also added results of statistic analysis on second dataset.

--- project/mlp3.py
+++ project/mlp3.py
@@ -1,23 +1,22 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
-import random
 
 seed = 7
 numpy.random.seed(seed)
-dataset = numpy.loadtxt("normalized_set.csv", delimiter=",")
+dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 
-x = dataset[:,1:9]
-y = dataset[:,9]
+x = dataset[:,0:4]
+y = dataset[:,5]
 
 model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(12, input_dim=4, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x, y, nb_epoch=150, batch_size=15, validation_split=.2)
+model.fit(x, y, nb_epoch=50, batch_size=10)
 
 scores = model.evaluate(x, y)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
